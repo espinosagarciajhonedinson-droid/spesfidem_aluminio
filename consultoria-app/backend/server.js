9783f66dfa329a6f420d22ef3ca1f619fed71c1e
Jhon Espinosa <jhonespinosa@email.com>
2026-03-25 12:56:44 -0500

    chore: full project cleanup - removed obsolete files, dependencies and build cache

--- consultoria-app/backend/server.js
+++ consultoria-app/backend/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
-const twilio = require('twilio');
 
 const app = express();
 app.use(cors());
